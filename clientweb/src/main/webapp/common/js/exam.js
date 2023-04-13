fa99e4a32ff49472b0c00d24788d7dc991baeb06
DojinP <coah2013@naver.com>
2023-04-14 00:29:28 +0900

    ~ 230413 커밋

---
+++ clientweb/src/main/webapp/common/js/exam.js
@@ -0,0 +1,11 @@
+function test1(){
+    alert("test1");
+}
+function test2(){
+    result = confirm("test2");
+    alert(result);
+}
+function test3(){
+    result = prompt("test3");
+    alert(result);
+}
